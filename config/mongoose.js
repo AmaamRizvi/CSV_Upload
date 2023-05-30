012a54c73e3c805adbb32123097189398cb3df1e
Amaam_Rizvi <amaamrizvi6557@gmail.com>
2023-05-31 00:53:26 +0530

    some updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,26 @@
+// const mongoose = require("mongoose");
+// // DycNZg9ED3sECkxf
+// mongoose.connect(
+//   "mongodb+srv://amaamrizvi6557:DycNZg9ED3sECkxf@cluster0.pxtxhrz.mongodb.net/"
+// );
+
+// const db = mongoose.connection;
+
+// db.once("open", function () {
+//   console.log("database connected to the server successfully!");
+// });
+
 const mongoose = require("mongoose");
-// DycNZg9ED3sECkxf
-mongoose.connect(
-  "mongodb+srv://amaamrizvi6557:DycNZg9ED3sECkxf@cluster0.pxtxhrz.mongodb.net/"
-);
 
-const db = mongoose.connection;
-
-db.once("open", function () {
-  console.log("database connected to the server successfully!");
-});
+const db = mongoose
+  .connect(
+    "mongodb+srv://amaamrizvi6557:DycNZg9ED3sECkxf@cluster0.pxtxhrz.mongodb.net/"
+  )
+  .then(() => {
+    console.log("database connected to the server successfully!");
+  })
+  .catch((e) => {
+    console.log(e, "not connected");
+  });
 
 module.exports = db;
